8f6269d204d333172f1e0fa6780d4f261dd8ff20
mike <mshrieve@gmail.com>
2023-07-13 17:41:59 -0700

    update tests

--- ts/test/prepare.test.ts
+++ ts/test/prepare.test.ts
@@ -1,4 +1,4 @@
-import { type Address, isAddress, type Hex } from "viem";
+import { type Hex, zeroAddress } from "viem";
 import { beforeAll, afterAll, expect, test, expectTypeOf } from "vitest";
 
 import { deployAll } from "./setup/utils/deployAll";
@@ -6,15 +6,18 @@ import { ALICE } from "./setup/constants";
 import { publicClient } from "./setup/clients";
 
 let contracts: Awaited<ReturnType<typeof deployAll>>;
+let marketId: Hex;
 
 beforeAll(async () => {
   contracts = await deployAll();
 });
 
 test("can prepare market", async () => {
-  const result = await contracts.negRiskAdapter.simulate.prepareMarket([BigInt(0), "0x"]);
+  const result = await contracts.negRiskAdapter.simulate.prepareMarket([BigInt(0), "0x"], { account: ALICE });
   expect(result).toBeDefined();
-  expectTypeOf(result.result).toMatchTypeOf<Hex>;
+
+  marketId = result.result;
+  expectTypeOf(marketId).toMatchTypeOf<Hex>;
 
   const prepareMarketHash = await contracts.negRiskAdapter.write.prepareMarket([BigInt(0), "0x"], {
     account: ALICE,
@@ -23,6 +26,26 @@ test("can prepare market", async () => {
   await publicClient.waitForTransactionReceipt({
     hash: prepareMarketHash,
   });
+
+  const oracleAddress = await contracts.negRiskAdapter.read.getOracle([marketId]);
+
+  expect(oracleAddress).to.not.equal(zeroAddress);
+});
+
+test("can prepare market and question", async () => {
+  const questionCountBefore = await contracts.negRiskAdapter.read.getQuestionCount([marketId]);
+  expect(questionCountBefore).toEqual(BigInt(0));
+
+  const prepareQuestionHash = await contracts.negRiskAdapter.write.prepareQuestion([marketId, "0x"], {
+    account: ALICE,
+  });
+
+  await publicClient.waitForTransactionReceipt({
+    hash: prepareQuestionHash,
+  });
+
+  const questionCountAfter = await contracts.negRiskAdapter.read.getQuestionCount([marketId]);
+  expect(questionCountAfter).toEqual(BigInt(1));
 });
 
 afterAll(async () => {
