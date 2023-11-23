8cfcf88a675c165be86c141b63c9939647a5a2a5
mike <mshrieve@gmail.com>
2023-11-22 19:45:50 -0500

    remove commented tests + extra NegRiskFeeModule approval

--- src/NegRiskFeeModule.sol
+++ src/NegRiskFeeModule.sol
@@ -7,7 +7,6 @@ import {IConditionalTokens} from "./interfaces/IConditionalTokens.sol";
 contract NegRiskFeeModule is FeeModule {
     constructor(address _negRiskCtfExchange, address _negRiskAdapter, address _ctf) FeeModule(_negRiskCtfExchange) {
         IConditionalTokens(_ctf).setApprovalForAll(_negRiskAdapter, true);
-        IConditionalTokens(_ctf).setApprovalForAll(_negRiskCtfExchange, true);
         IConditionalTokens(_ctf).setApprovalForAll(address(this), true);
     }
 }
