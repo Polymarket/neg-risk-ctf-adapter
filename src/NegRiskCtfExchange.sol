274d3f7338a8222fa61461c5b0b87236a84699d4
mike <mshrieve@gmail.com>
2023-11-27 11:29:29 -0800

    add natspec

--- src/NegRiskCtfExchange.sol
+++ src/NegRiskCtfExchange.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.15;
 import {CTFExchange} from "../lib/ctf-exchange/src/exchange/CTFExchange.sol";
 import {IConditionalTokens} from "./interfaces/IConditionalTokens.sol";
 
+/// @title NegRiskCtfExchange
+/// @notice A slightly modified version of CTFExchange
+/// @notice with added approvals for the NegRiskAdapter
 contract NegRiskCtfExchange is CTFExchange {
     constructor(address _collateral, address _ctf, address _negRiskAdapter, address _proxyFactory, address _safeFactory)
         CTFExchange(_collateral, _negRiskAdapter, _proxyFactory, _safeFactory)
